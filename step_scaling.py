0f6977cf27efa97d042e634bf0a1b8dd543fefb4
Andrew Lytle <atlytle+github@gmail.com>
2011-08-13 21:09:02 +0100

    step_scaling.py: Use dictionaries for different schemes.

--- step_scaling.py
+++ step_scaling.py
@@ -11,49 +11,22 @@ def sigma(Zf, Zi):
 
 def step_scale(Data_f, Data_i):
     "Sigma in all four schemes."
-    Data_f.step_scale = sigma(Data_f.fourquark_Zs, Data_i.fourquark_Zs)
-    Data_f.step_scale_q = sigma(Data_f.fourquark_Zs_q, Data_i.fourquark_Zs_q)
-    Data_f.step_scale_qg = sigma(Data_f.fourquark_Zs_qg, Data_i.fourquark_Zs_qg)
-    Data_f.step_scale_qq = sigma(Data_f.fourquark_Zs_qq, Data_i.fourquark_Zs_qq)
-
+    Data_f.step_scale = {}
+    for scheme in 'gg', 'gq', 'qg', 'qq':
+        Data_f.step_scale[scheme] = sigma(Data_f.fourquark_Zs[scheme],
+                                          Data_i.fourquark_Zs[scheme])
 
 def step_scale_sigma(Data_f, Data_i):
     '''Naive error propagation for step-scaling functions.'''
-    # (g, g)
-    Zf = Data_f.fourquark_Zs
-    Zi = Data_i.fourquark_Zs
-    dZf = (Data_f.fourquark_sigmaJK)*(Data_f.fourquark_sigmaJK)
-    dZi_inv = reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK, inv(Zi)])*\
-              reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK, inv(Zi)])
-    dSS = sqrt(dot(dZf, inv(Zi)) + dot(Zf, dZi_inv))
-    Data_f.step_scale_sigma = dSS
-
-    # (g, q)
-    Zf = Data_f.fourquark_Zs_q
-    Zi = Data_i.fourquark_Zs_q
-    dZf = (Data_f.fourquark_sigmaJK_q)*(Data_f.fourquark_sigmaJK_q)
-    dZi_inv = reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK_q, inv(Zi)])*\
-              reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK_q, inv(Zi)])
-    dSS = sqrt(dot(dZf, inv(Zi)) + dot(Zf, dZi_inv))
-    Data_f.step_scale_sigma_q = dSS
-
-   # (q, g)
-    Zf = Data_f.fourquark_Zs_qg
-    Zi = Data_i.fourquark_Zs_qg
-    dZf = (Data_f.fourquark_sigmaJK_qg)*(Data_f.fourquark_sigmaJK_qg)
-    dZi_inv = reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK_qg, inv(Zi)])*\
-              reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK_qg, inv(Zi)])
-    dSS = sqrt(dot(dZf, inv(Zi)) + dot(Zf, dZi_inv))
-    Data_f.step_scale_sigma_qg = dSS
-
-   # (q, q)
-    Zf = Data_f.fourquark_Zs_qq
-    Zi = Data_i.fourquark_Zs_qq
-    dZf = (Data_f.fourquark_sigmaJK_qq)*(Data_f.fourquark_sigmaJK_qq)
-    dZi_inv = reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK_qq, inv(Zi)])*\
-              reduce(dot, [inv(Zi), Data_i.fourquark_sigmaJK_qq, inv(Zi)])
-    dSS = sqrt(dot(dZf, inv(Zi)) + dot(Zf, dZi_inv))
-    Data_f.step_scale_sigma_qq = dSS
+    Data_f.step_scale_sigma = {}
+    for scheme in 'gg', 'gq', 'qg', 'qq':    
+        Zf = Data_f.fourquark_Zs[scheme]
+        Zi = Data_i.fourquark_Zs[scheme]
+        dZf = (Data_f.fourquark_sigmaJK[scheme])**2
+        tmp = [inv(Zi), Data_i.fourquark_sigmaJK[scheme], inv(Zi)]
+        dZi_inv = reduce(dot, tmp)**2
+        dSS = sqrt(dot(dZf, inv(Zi)) + dot(Zf, dZi_inv))
+        Data_f.step_scale_sigma[scheme] = dSS
 
 def step_scale_JK(Data_f, Data_i):
     pass
