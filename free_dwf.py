ccf48bc0b629ec1f11098c6eb9edcf689cdacb5d
Andrew Lytle <atlytle+github@gmail.com>
2011-11-04 16:11:17 +0000

    Added free_dwf.py

---
+++ free_dwf.py
@@ -0,0 +1,43 @@
+import math
+from numpy import dot
+
+def phat(p):
+    hat = lambda p: 2*math.sin(p/2.)
+    return map(hat, p)
+
+def pbar(p):
+    bar = lambda p: math.sin(p)
+    return map(bar, p)
+
+def W(p, M):
+    ph = phat(p)
+    return 1 - M + dot(ph, ph)/2.
+
+def ea(p, M):
+    w = W(p, M)
+    pb = pbar(p)
+    pb_sq = dot(pb, pb)
+    ch = 1 + w*w + pb_sq/(2.*abs(w))
+    return ch + math.sqrt(ch*ch-1)
+
+def xx(p, M):
+    w = W(p, M)
+    pb = pbar(p)
+    pb_sq = dot(pb, pb)
+    return (1 + w*w + pb_sq)/(2.*abs(w))
+
+def yy(p, M):
+    w = W(p, M)
+    pb = pbar(p)
+    pb_sq = dot(pb, pb)
+    return 1 + w*w + pb_sq
+
+def zz(p, M):
+    w = W(p, M)
+    y = yy(p, M)
+    return y/2. + math.sqrt(y*y/4. - w*w)
+
+
+        
+
+    
