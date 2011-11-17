c20e7f1b1288e5a73e088903011a2631fe3f0c3d
Andrew Lytle <atlytle+github@gmail.com>
2011-11-17 11:58:18 +0000

    free_dwf.py: Gammas and pslash.

--- free_dwf.py
+++ free_dwf.py
@@ -1,5 +1,6 @@
-import math
-from numpy import dot
+import math, sys
+import numpy as np
+from numpy import array, dot
 
 # dwf-function-defs.nb
 def phat(p):
@@ -39,16 +40,50 @@ def zz(p, M):
     return y/2. + math.sqrt(y*y/4. - w*w)
 
 # free-dwf-propagator.nb
-def pslash(p):
-    pass
+g = [None for x in range(6)]
+g[0] = array([[0, 0, 1, 0],
+              [0, 0, 0, 1],
+              [1, 0, 0, 0],
+              [0, 1, 0, 0]])
+g[1] = array([[0, 0, 0, -1j],
+              [0, 0, -1j, 0],
+              [0, 1j, 0, 0],
+              [1j, 0, 0, 0]])
+g[2] = array([[0, 0, 0, -1],
+              [0, 0, 1, 0],
+              [0, 1, 0, 0],
+              [-1, 0, 0, 0]])
+g[3] = array([[0, 0, -1j, 0],
+              [0, 0, 0, 1j],
+              [1j, 0, 0, 0],
+              [0, -1j, 0, 0]])
+g[4] = g[0]
+g[5] = reduce(dot, [g[1], g[2], g[3], g[4]])
+id4 = np.identity(4)
+
+def pslash(p):  # Note different index convention!
+    return sum([p[i]*g[i] for i in range(4)])
 
 def prop(p, m, M, Ls):
     '''Free momentum space propagator, S(p).'''
-    pass
+    pbar_sl = pslash(pbar(p))
+    w = W(p, M)
+    z = zz(p, M)
+    owe = 1 - w*w/z
+    ea = z/w #dif from ea(...)?
+    fac = (z-w*w/z)/(ea**(2*N)-1)
+    f = 1 - z - m*m*owe + fac*(2*m*ea**N - 1 - m*m)
+    return (-1j*pbar_sl + m*owe*id4)/(z + m*m*owe - 1) #def id4!
 
 def prop_x(x, p, m, M, Ls):
     "S(x, p)"
     pass
         
+# main
+def main():
+    L, T, M, m = 4, 8, 1.8, 1.9
+    return 0
 
+if __name__ == "__main__":
+    sys.exit(main())
     
